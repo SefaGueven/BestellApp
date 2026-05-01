ba2995cbb983aefed32853b28c71179e9ede5f23
Sefa Güven <sefagueven003@gmail.com>
2026-05-01 17:32:15 +0200

    Menu in Switch Case funktion umgestellt ,footer hinzugefügt.

--- script.js
+++ script.js
@@ -7,13 +7,36 @@ function renderMenu() {
 
         for (let dishIndex = 0; dishIndex < dishesList.length; dishIndex++) {
             const dish = dishesList[dishIndex];   
-            if (currentCategory === 'burger') {
+            switch (currentCategory) {
+                case 'burger':
                 document.getElementById("menuBurger").innerHTML += templateMenuItem(dish, 'burger', dishIndex);
-            } else if (currentCategory === 'pizza') {
+                break;
+
+            case 'pizza':
                 document.getElementById("menuPizza").innerHTML += templateMenuItem(dish, 'pizza', dishIndex);
-            } else if (currentCategory === 'salad') {
+                break;
+
+            case 'salad':
                 document.getElementById("menuSalad").innerHTML += templateMenuItem(dish, 'salad', dishIndex);
+                break;
             }
         }
     }
+}
+
+function addToOrder(category, index) {
+    let item = menues.find(cat => cat.category === category).dishes[index];
+    let menuToAdd = {
+        name: item.name,
+        price: item.price,
+        amount: 1
+    };
+        let existingItem = orderItems.find(orderItem => orderItem.name === menuToAdd.name);
+
+    if (existingItem) {
+        existingItem.amount++;
+    } else {
+        orderItems.push(menuToAdd);
+    }
+    renderOrder();
 }
