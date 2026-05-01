5fb475469db76dd7d034eabd990a755778a1d536
Sefa Güven <sefagueven003@gmail.com>
2026-05-01 16:51:10 +0200

    ordner Script erstellt mit db.js und template.js

---
+++ script/db.js
@@ -0,0 +1,89 @@
+let menues = [
+    {
+        category: "burger",
+        dishes: [
+            {
+                image: "./assets/pics/veggie-msh-bbg.jpg",
+                name: "Veggie Mushroom Black Burger",
+                description: "Mixed green Salad, Tomatoes, Edamame, Mushrooms",
+                price: 16.90
+            },
+            {
+                image: "./assets/pics/Allmeat-brg.jpg",
+                name: "All Meat Burger",
+                description: "Beef, Bacon, Dill Pickles, Smoked Cheese, Ketchup, BBQ Sauce",
+                price: 15.90
+            },
+            {
+                image: "./assets/pics/redburger.jpg",
+                name: "Beef Red Burger",
+                description: "Beef, Cheddar Cheese, Lettuce, Tomato, Onion, BBQ Sauce",
+                price: 14.90
+            },
+            {
+                image: "./assets/pics/chicburger.jpg",
+                name: "Big Chicken Burger",
+                description: "Chicken, Cheese, Lettuce, Tomato, Onion, Mayo, Bell Peppers",
+                price: 15.90
+            }
+        ]
+    },
+    {
+        category: "pizza",
+        dishes: [
+            {
+                image: "./assets/pics/margheritta.jpg",
+                name: "Pizza Margherita",
+                description: "Tomato Sauce, Mozzarella Cheese, Fresh Basil",
+                price: 11.90
+            },
+            {
+                image: "./assets/pics/chorizo.jpg",
+                name: "Pizza Chorizo",
+                description: "Tomato Sauce, Mozzarella Cheese, Chorizo, Red Onion",
+                price: 13.90
+            },
+            {
+                image: "./assets/pics/funghi.jpg",
+                name: "Pizza Funghi",
+                description: "Tomato Sauce, Red Onions, Olive,  Mozzarella Cheese, Mushrooms",
+                price: 12.90
+            },
+            {
+                image: "./assets/pics/4formaggi.jpg",
+                name: "Quatro Formaggi with Chicken",
+                description: "Tomato Sauce, Mozzarella Cheese, Gorgonzola, Parmigiano Reggiano, Fontina, Emmental, Chicken",
+                price: 15.90
+            }
+        ]
+    },
+    {
+        category: "salad",
+        dishes: [
+            {
+                image: "./assets/pics/arugula.jpg",
+                name: "Warm Beef Arugula Salad",
+                description: "Beef, Arugula, Field Salad, Greek Feta, Cherry Tomatoes, Sun-dried Tomatoes, Balsamic-vinegar Dressing",
+                price: 16.90
+            },
+            {
+                image: "./assets/pics/minigreen.jpg",
+                name: "Mini Green Salad",
+                description: "Mixed Greens, Carrot, Radishes, Cherry Tomatoes, Cucumber, Balsamic Dressing",
+                price: 7.90
+            },
+            {
+                image: "./assets/pics/seafoodsalad.jpg",
+                name: "Green Salad with Seafood",
+                description: "Mixed Greens, Cherry Tomatoes, Red Onions, Mussels, Squid Ring, Shrimp, Dijon Mustard-lemon Dressing with Dill",
+                price: 16.90
+            },
+            {
+                image: "./assets/pics/saladtofu.jpg",
+                name: "Vegan Green Salad with Tofu",
+                description: "Green Salad, Cherry Tomatoes, Cucumber, Baby Spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+                price: 14.90
+            }
+        ]
+    }
+];
